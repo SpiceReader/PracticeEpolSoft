3ad1008344c4be369d60e429ff0829f4fe04811c
Demchuk Tatyana <69cobak@mail.ru>
2020-06-23 01:19:51 +0300

    implement interface mainWindow

--- GUI/mainwindow.cpp
+++ GUI/mainwindow.cpp
@@ -16,11 +16,78 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_saveButton_clicked()
 {
+    if(ui->taskDescription->toPlainText().isEmpty() ||
+            ui->taskName->text().isEmpty() ||
+            !isRadiosChecked()) return;
 
+    Task task;
+    QString taskTitle = ui->taskName->text();
+    QString taskDescription = ui->taskDescription->toPlainText();
+    QGroupBox *taskBox = new QGroupBox(taskTitle);
+    QLabel *LabelTaskDescription = new QLabel(taskDescription);
+    QVBoxLayout *taskLayout = new QVBoxLayout;
+
+    taskBox->setMinimumHeight(80);
+
+    task.setTaskName(taskTitle.toStdString());
+    task.setTaskDescription(ui->labelTaskDescription->text().toStdString());
+
+
+    taskLayout->addWidget(LabelTaskDescription);
+
+    taskBox->setLayout(taskLayout);
+    LabelTaskDescription->setWordWrap(true);
+
+    if(ui->radioOpen->isChecked()){
+        ui->openTasksLayout->addWidget(taskBox);
+    }
+    if(ui->radioProcess->isChecked()){
+        ui->proccesTaskLayout->addWidget(taskBox);
+    }
+
+    if(ui->radioClosed->isChecked()){
+        ui->closeTaskLayout->addWidget(taskBox);
+    }
+
+    if(ui->radioVerification->isChecked()){
+        ui->verTaskLayout->addWidget(taskBox);
+    }
+
+    SingletonTaskHandler::getInstance()->addTask(task);
 }
 
 void MainWindow::on_clearButton_clicked()
 {
     ui->taskName->setText("");
     ui->taskDescription->setText("");
+
+    SingletonTaskHandler::getInstance()->clearTasks();
+
+    clearLayout(ui->openTasksLayout, 2);
+    clearLayout(ui->proccesTaskLayout, 2);
+    clearLayout(ui->verTaskLayout, 2);
+    clearLayout(ui->closeTaskLayout, 2);
 }
+
+void MainWindow::clearLayout(QLayout *layout, int numWidget){
+    QLayoutItem *item;
+        while((item = layout->takeAt(numWidget))) {
+            if (item->layout()) {
+                clearLayout(item->layout(), numWidget);
+                delete item->layout();
+            }
+            if (item->widget()) {
+               delete item->widget();
+            }
+            delete item;
+        }
+}
+
+bool MainWindow::isRadiosChecked(){
+    return (ui->radioOpen->isChecked() ||
+            ui->radioClosed->isChecked() ||
+            ui->radioVerification->isChecked() ||
+            ui->radioProcess->isChecked());
+}
+
+
