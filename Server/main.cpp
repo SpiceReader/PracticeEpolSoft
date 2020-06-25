f9cc7258fee9beff29d068c3798ad76e4eaf7d4a
Ihar Antonik <webmaster@svjatogor.by>
2020-06-26 02:48:42 +0300

    Integrate conan

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,11 +1,22 @@
 #include "mainwindow.h"
 
+#include <string>
 #include <QApplication>
+#include <json/json.h>
+#include <fstream>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    std::ifstream json_file("jsonExample.txt");
+    Json::Value jsonValue;
+    json_file >> jsonValue;
+    json_file.close();
+
+    std::cout << jsonValue["arguments"] << std::endl;
+
+    /*QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    return a.exec();
+    return a.exec();*/
 }
