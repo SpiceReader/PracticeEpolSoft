f796a2c01c80c40ea8df4769e545b331af24b557
Ihar Antonik <ihan@epolsoft.com>
2020-06-26 20:42:29 +0300

    Add example of JsonCpp

--- Server/main.cpp
+++ Server/main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <filesystem>
+#include <vector>
 #include <Windows.h>
 
 int main(int argc, char *argv[])
@@ -21,7 +22,6 @@ int main(int argc, char *argv[])
         SetCurrentDirectoryA(urlK.c_str());
     }
 
-
     std::ifstream file;
     file.open("jsonExample.txt");
     if(file.is_open())
@@ -30,7 +30,69 @@ int main(int argc, char *argv[])
         file >> jsonValue;
         file.close();
 
-        std::cout << jsonValue << std::endl;
+        unsigned int commandCode = jsonValue["commandCode"].asUInt();
+        Json::Value arguments = jsonValue["arguments"];
+
+        std::cout << "Command code: " << commandCode << std::endl;
+        std::cout << "Arguments: " << arguments << std::endl;
+
+        std::string string_argument = arguments["string_argument"].asString();
+
+        std::cout << "string_argument: " << string_argument << std::endl;
+
+        Json::Value array_argument = arguments["array_argument"];
+        for (unsigned int i = 0; i < array_argument.size(); i++)
+        {
+            std::string current = array_argument.get(i, "").asString();
+            std::cout << current << std::endl;
+        }
+
+        std::vector<std::string> memberNames = jsonValue.getMemberNames();
+
+        std::cout << "Member names" << std::endl;
+
+        for (std::string cur : memberNames)
+        {
+            std::cout << cur << std::endl;
+        }
+
+        if (jsonValue.isObject())
+        {
+            std::cout << "Json value is object" << std::endl;
+        }
+
+        if (jsonValue.isString())
+        {
+            std::cout << "Json value is string" << std::endl;
+        }
+        else
+        {
+            std::cout << "Json value is not a string" << std::endl;
+        }
+
+        jsonValue["floatArgument"] = 1.25;
+
+        jsonValue["emptyObject"] = Json::Value(Json::ValueType::objectValue);
+
+        jsonValue["arrayObject"] = Json::Value(Json::arrayValue);
+
+        jsonValue["arrayObject"].append("string");
+        jsonValue["arrayObject"].append(1);
+        jsonValue["arrayObject"].append(Json::Value(Json::objectValue));
+
+        std::cout << "After new field is added: " << jsonValue << std::endl;
+
+        Json::FastWriter writer;
+        std::string json = writer.write(jsonValue);
+
+        std::cout << "Json as string: " << json << std::endl;
+
+        Json::Reader reader;
+        Json::Value valueAfterParsing;
+
+        reader.parse(json, valueAfterParsing);
+
+        std::cout << "Json after conversion: " << valueAfterParsing << std::endl;
     }
 
     /*QApplication a(argc, argv);
