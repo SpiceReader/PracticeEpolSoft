53de541232ef71278df44c4761fdf3bc56e91403
AS005214 <arti1448coolio@gmail.ru>
2020-10-23 10:32:28 +0300

    init commit

--- Client/MessageHandler/MessageHandler.h
+++ Client/MessageHandler/MessageHandler.h
@@ -23,15 +23,8 @@ class MessageHandler : public QMessageBox
     Q_OBJECT
 
 public:
-    /**
-        * Returns singleton instance of MessageHandler
-        * @return message handler instance
-    */
-    static MessageHandler* getInstance();
 
-    /**
-        * Deletes instance of MessageHandler. Used to clear memory when program terminates
-    */
+    static MessageHandler* getInstance();
     static void deleteInstance();
 
     /**
@@ -42,7 +35,6 @@ public:
         * @param request request to be sent to server
         * @param observer IMessageObserver to notify sender of message about answer from server
     */
-
     void sendMessage(Request request, IMessageObserver* observer);
     //void sendMessage();
     void setServerIp(std::string serverIp);
@@ -68,12 +60,6 @@ private:
     Response getResponce() const;
 
     void setResponce(Response resp);
-
-    // Реализуйте методы сохранения и получения ip и порта в этом классе
-    //getServerIp();
-    //setServerIp(ip);
-    //unsigned int getServerPort() const;
-    //void setServerIp(unsigned int serverIp);
 
 };
 
--- Client/TaskHandler/SingletonTaskHandler.h
+++ Client/TaskHandler/SingletonTaskHandler.h
@@ -20,45 +20,12 @@
 class SingletonTaskHandler
 {
 public:
-    /**
-     * @return instance of SingletonTaskHandler
-     */
     static SingletonTaskHandler* getInstance();
-
-    /**
-     * @brief Deletes instance of SingletonTaskHandler
-     *        Usually used at the end of program execution
-     */
     static void deleteInstance();
-
-    /**
-     * @brief Adds task with provided ID to list of tasks
-     * @param task Task to be added
-     * @return true if task was added, false if task with such ID already exist
-     */
     bool addTask(const Task& task);
-
-    /**
-     * @brief Updates task with provided ID in list of tasks
-     * @param task Task to be updated
-     * @return true if task is exist and updated, false otherwise
-     */
     bool updateTask(const Task& task);
-
-    /**
-     * @brief Deletes task with provided ID from list of tasks
-     * @param id Id of task to be deleted
-     */
     void deleteTaskById(unsigned int id);
-
-    /**
-     * @brief clears vector of tasks
-     */
     void clearTasks();
-
-    /**
-     * @return vector of tasks
-     */
     std::vector<Task> getTasks() const;
 
 private:
